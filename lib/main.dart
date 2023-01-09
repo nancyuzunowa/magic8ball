4b611a6de142b7e3462c32552f638c093455786a
Nansi Uzunova <nansi.uzunova@imperiaonline.org>
2023-01-09 17:24:03 +0200

    Created a stateful widget with image in the center.

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,24 @@ class BallPage extends StatelessWidget {
         title: new Text('Ask Me Anything'),
         backgroundColor: Colors.blue[900],
       ),
-      body: new Container(),
+      body: new Ball(),
       backgroundColor: Colors.blue,
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
     );
   }
 }
