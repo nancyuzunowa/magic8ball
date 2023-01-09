54e87b80c5a461aa446d69c13866b9e6fd8db59a
Nansi Uzunova <nansi.uzunova@imperiaonline.org>
2023-01-09 17:20:28 +0200

    Created a stateless widget.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: new BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: new Container(),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
